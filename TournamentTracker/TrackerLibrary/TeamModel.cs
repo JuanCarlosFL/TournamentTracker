c5ecbb70f7ea14377690757e9e81576e60c21765
Juan Carlos <jcfmalaga@gmail.com>
2020-06-04 23:32:46 +0200

    class library implemented

---
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>(); //The same if i do this in the constructor
+
+        public string TeamName { get; set; }
+
+    }
+}
